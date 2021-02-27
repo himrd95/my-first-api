91b7196fd760a2d8aff1d46f649d89de8c925f54
Himanshu <himrd95@gmail.com>
2021-02-27 10:21:49 +0530

    api added

---
+++ app.js
@@ -0,0 +1,52 @@
+const dataBase = require('./db.json');
+let books = dataBase.books
+const express = require("express");
+const app = express();
+
+app.use(logger, express.json())
+
+
+app.get("/books", (req, res) => {
+    
+    res.send({"API requested by" : req.name, "books": books})
+})
+app.get("/books/:id", (req, res) => {
+    const id = +req.params.id;
+    const specificBook = books.find(book=>book.id === id)
+    res.send({"API requested by" : req.name, "book": specificBook})
+})
+app.post("/books", (req, res) => {
+    books.push(req.body)
+    res.status(201).json(req.body)
+})
+app.put("/books/:id", (req, res) => {
+    const id = +req.params.id;
+    const userBody = req.body
+    const updated = books.map(book => book.id === id ? userBody : book)
+    books = updated
+    res.send(books)
+})
+app.patch("/books/:id", (req, res) => {
+    const id = +req.params.id;
+    const userBody = req.body
+    const updated = books.map(book =>
+        book.id === id ? { ...book, ...userBody } : book)
+    books = updated
+    res.status(200).json(updated)
+})
+app.delete("/books/:id", (req, res) => {
+    const id = +req.params.id;
+    const updated = books.filter(book => book.id !== id )
+    books = updated
+    res.send(books)
+})
+
+function logger(req, res, next) {
+    req.name = "Himanshu Dwivedi"
+    next();
+}
+
+
+app.listen(8000, () => {
+    console.log("Listening on 8000 port")
+})
